5c18c691257552fe6e4e7c5e2614e5ac95a6eef7
selim-karaduman <selim.karaduman07@gmail.com>
2020-12-09 14:58:34 +0300

    add dockerfiles and setup script

---
+++ morphological_parser_sak/install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -euo pipefail
+export DEBIAN_FRONTEND=noninteractive
+apt-get update
+apt-get -y upgrade
+apt-get -y install --no-install-recommends git
+apt-get clean
+rm -rf /var/lib/apt/lists/*
